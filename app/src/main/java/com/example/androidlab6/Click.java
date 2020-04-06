097ff97ad889c1ec80eb37d884cc5d6303b5a88e
svartvalp <kasyanenko.konstantin@mail.ru>
2020-04-06 18:51:54 +0300

    got it

---
+++ app/src/main/java/com/example/androidlab6/Click.java
@@ -0,0 +1,5 @@
+package com.example.androidlab6;
+
+public interface Click {
+    public void click(int position);
+}
---
+++ app/src/main/java/com/example/androidlab6/DataChangedListener.java
@@ -0,0 +1,5 @@
+package com.example.androidlab6;
+
+public interface DataChangedListener {
+    public void notifyDataChanged();
+}
